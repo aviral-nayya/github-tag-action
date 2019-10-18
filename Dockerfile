a6ef772d37b497db4490066bd13bbaa1122ba3c9
w.jurasz <w.jurasz@kigroup.de>
2019-10-18 13:05:43 +0200

    Added latest ta

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM alpine
-LABEL "repository"="https://github.com/anothrNick/github-tag-action"
-LABEL "homepage"="https://github.com/anothrNick/github-tag-action"
-LABEL "maintainer"="Nick Sjostrom"
+LABEL "repository"="https://github.com/WiktorJ/github-tag-action"
+LABEL "homepage"="https://github.com/WiktorJ/github-tag-action"
+LABEL "maintainer"="Wiktor Jurasz"
+LABEL "forked"="https://github.com/anothrNick/github-tag-action"
 
 COPY ./contrib/semver ./contrib/semver
 RUN install ./contrib/semver /usr/local/bin
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,63 +1,59 @@
 #!/bin/bash
 
 # config
-default_semvar_bump=${DEFAULT_BUMP:-minor}
+default_semvar_bump=${DEFAULT_BUMP:-none}
 with_v=${WITH_V:-false}
-
 # get latest tag
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 tag_commit=$(git rev-list -n 1 $tag)
-
 # get current commit hash for tag
 commit=$(git rev-parse HEAD)
-
-if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    exit 0
-fi
-
-# if there are none, start tags at 0.0.0
-if [ -z "$tag" ]
-then
-    log=$(git log --pretty=oneline)
-    tag=0.0.0
-else
-    log=$(git log $tag..HEAD --pretty=oneline)
-fi
-
-# get commit logs and determine home to bump the version
-# supports #major, #minor, #patch (anything else will be 'minor')
-case "$log" in
-    *#major* ) new=$(semver bump major $tag);;
-    *#minor* ) new=$(semver bump minor $tag);;
-    *#patch* ) new=$(semver bump patch $tag);;
-    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
-esac
-
-# prefix with 'v'
-if $with_v
-then
-    new="v$new"
-fi
-
-echo $new
-
-# set output
-echo ::set-output name=new_tag::$new
-
-# push new tag ref to github
-dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
-full_name=$GITHUB_REPOSITORY
 git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+if [ "$default_semvar_bump" != "none" ]; then
+	if [ "$tag_commit" == "$commit" ]; then
+	    echo "No new commits since previous tag. Skipping..."
+	    exit 0
+	fi
 
-echo "$dt: **pushing tag $new to repo $full_name"
+	# if there are none, start tags at 0.0.0
+	if [ -z "$tag" ]
+	then
+	    log=$(git log --pretty=oneline)
+	    tag=0.0.0
+	else
+	    log=$(git log $tag..HEAD --pretty=oneline)
+	fi
 
-curl -s -X POST $git_refs_url \
--H "Authorization: token $GITHUB_TOKEN" \
--d @- << EOF
+	# get commit logs and determine home to bump the version
+	# supports #major, #minor, #patch (anything else will be 'minor')
+	case "$log" in
+	    *#major* ) new=$(semver bump major $tag);;
+	    *#minor* ) new=$(semver bump minor $tag);;
+	    *#patch* ) new=$(semver bump patch $tag);;
+	    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
+	esac
 
-{
-  "ref": "refs/tags/$new",
-  "sha": "$commit"
-}
-EOF
+	# prefix with 'v'
+	if $with_v
+	then
+	    new="v$new"
+	fi
+	echo $new
+
+	# set output
+	echo ::set-output name=new_tag::$new
+
+	# push new tag ref to github
+	dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
+	full_name=$GITHUB_REPOSITORY
+
+	echo "$dt: **pushing tag $new to repo $full_name"
+
+	curl -s -X POST $git_refs_url \
+	-H "Authorization: token $GITHUB_TOKEN" \
+	-d '{"ref": "refs/tags/$new", "sha": "$commit"}'
+else
+	curl -s -X POST $git_refs_url \
+	-H "Authorization: token $GITHUB_TOKEN" \
+	-d '{"ref": "refs/tags/latest", "sha": "$commit"}'
+fi
